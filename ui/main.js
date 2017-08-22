7c6b2fdb8f63f8a44dd6e7266806b0b27371ee75
Anjani R Bhat <anjani897bhat@gmail.com>
2017-08-22 22:03:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 // change text of 'main' div
 var element = document.getElementById('main');
 element.innerHTML='New Value'
+
+//move the image
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft="100px";
+}
